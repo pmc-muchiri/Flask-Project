0423a50faf03b4074fcf8d3f89929729b0122bdb
pmc-muchiri <pmc.ac.ke@gmail.com>
2022-07-23 01:27:05 +0300

    first commit

---
+++ website/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from httpx import Auth
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'fhsfsfufhiudgdididi'
+
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    return app
